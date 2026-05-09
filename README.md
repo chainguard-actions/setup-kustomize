d0bbb89e713cba008c97f35f8bf3226204457d67
actionchain <actionchain@chainguard.dev>
2026-05-09 11:50:11 +0000

    Update imranismail/setup-kustomize@v2.0.0

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# setup-kustomize
+![build-test](https://github.com/imranismail/setup-kustomize/workflows/build-test/badge.svg)
+
+## Description
+
+Install any kustomize version as a step in your workflow
+
+## Options
+
+Every argument is optional.
+
+| Input               | Description                                                                                                                                                                                                                             |
+| ------------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `github-token`      | PAT (Personal Access Token) for authorizing the repository.<br>_Defaults to **\${{ github.token }}**_                                                                                                                                   |
+| `kustomize-version` | Semver of kustomize to use. Examples: `10.x`, `10.15.1`, `>=10.15.0`<br>_Defaults to **\***_                                                                                                                                            |
+| `fail-fast`         | When github rate limits us, fail immediately or retry after the timeout that github wishes from us? <br>Note: When this is set to `false`, a github workflow might accrue a long (and possibly expensive) runtime.<br>_Defaults to **true**_ |
+
+## Usage
+
+```yaml
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  create-deployment-branch:
+    runs-on: ubuntu-latest
+    needs:
+      - publish-image
+    steps:
+      - uses: imranismail/setup-kustomize@v1
+      - run: |
+          kustomize edit set image app:${GITHUB_SHA}
+          git add .
+          git commit -m "Set `app` image tag to `${GITHUB_SHA}`"
+          git push
+```
+
+## Privacy
+
+This Action contacts Chainguard's licensing server to verify authorization. Connection metadata (IP address, GitHub repository identifier, timestamp, and any metadata encoded in the auth token) is transmitted to Chainguard, Inc. even if authorization is denied in accordance with our [Privacy Notice](https://www.chainguard.dev/legal/privacy-notice)
---
+++ src/installer.ts
@@ -0,0 +1,162 @@
+import {GitHub, getOctokitOptions} from '@actions/github/lib/utils'
+import {OctokitOptions} from '@octokit/core/dist-types/types'
+import {throttling} from '@octokit/plugin-throttling'
+import * as core from '@actions/core'
+import * as cache from '@actions/tool-cache'
+import * as path from 'path'
+import * as semver from 'semver'
+import * as fs from 'fs'
+let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || ''
+
+const EnhancedOctokit = GitHub.plugin(throttling)
+
+const githubToken = core.getInput('github-token')
+const failFast = core.getBooleanInput('fail-fast')
+
+let options: OctokitOptions = {
+  throttle: {
+    onRateLimit: (retryAfter: Number, opts: OctokitOptions) => {
+      core.warning(
+        `Request quota exhausted for request ${opts.method} ${opts.url}`
+      )
+      if (!failFast) {
+        core.warning(`Retrying after ${retryAfter} seconds!`)
+      }
+      return !failFast
+    },
+    onAbuseLimit: (retryAfter: Number, opts: OctokitOptions) => {
+      core.warning(`Abuse detected for request ${opts.method} ${opts.url}`)
+      if (!failFast) {
+        core.warning(`Retrying after ${retryAfter} seconds!`)
+      }
+      return !failFast
+    }
+  }
+}
+
+if (process.env.NODE_ENV === 'test') {
+  options = githubToken ? getOctokitOptions(githubToken, options) : options
+} else {
+  options = getOctokitOptions(githubToken, options)
+}
+
+const octokit = new EnhancedOctokit(options)
+const versionRegex = /\d+\.?\d*\.?\d*/
+const toolName = 'kustomize'
+const platform = process.platform
+const arch = process.arch === 'x64' ? 'amd64' : process.arch
+
+if (!tempDirectory) {
+  let baseLocation
+  if (process.platform === 'win32') {
+    // On windows use the USERPROFILE env variable
+    baseLocation = process.env['USERPROFILE'] || 'C:\\'
+  } else {
+    if (process.platform === 'darwin') {
+      baseLocation = '/Users'
+    } else {
+      baseLocation = '/home'
+    }
+  }
+  tempDirectory = path.join(baseLocation, 'actions', 'temp')
+}
+
+export async function getKustomize(targetVersion: string): Promise<void> {
+  if (!semver.validRange(targetVersion))
+    throw new Error(`invalid semver requested: ${targetVersion}`)
+
+  let kustomizePath = cache.find('kustomize', targetVersion)
+
+  if (!kustomizePath) {
+    const version = await getMaxSatisfyingVersion(targetVersion)
+    kustomizePath = await acquireVersion(version)
+  }
+
+  return core.addPath(kustomizePath)
+}
+
+interface Version {
+  resolved: string
+  target: string
+  url: string
+}
+
+async function getMaxSatisfyingVersion(
+  targetVersion: string
+): Promise<Version> {
+  const version = {target: targetVersion}
+  const availableVersions: Map<string, string> = new Map()
+
+  for await (const response of octokit.paginate.iterator(
+    octokit.repos.listReleases,
+    {
+      owner: 'kubernetes-sigs',
+      repo: 'kustomize',
+      per_page: 100
+    }
+  )) {
+    for (const release of response.data) {
+      const matchingAsset = release.assets.find(
+        asset =>
+          asset.name.includes('kustomize') &&
+          asset.name.includes(platform) &&
+          asset.name.includes(arch)
+      )
+
+      if (matchingAsset) {
+        const kustomizeVersion = (
+          versionRegex.exec(release.tag_name) || []
+        ).shift()
+
+        if (kustomizeVersion != null) {
+          availableVersions.set(
+            kustomizeVersion,
+            matchingAsset.browser_download_url
+          )
+        }
+      }
+    }
+  }
+
+  const resolved = semver.maxSatisfying(
+    [...availableVersions.keys()],
+    version.target
+  )
+
+  if (!resolved) {
+    throw new Error(
+      `Unable to find Kustomize version '${version.target}' for platform '${platform}' and architecture ${arch}.`
+    )
+  }
+
+  const url = availableVersions.get(resolved) as string
+
+  return {...version, resolved, url}
+}
+
+async function acquireVersion(version: Version): Promise<string> {
+  const toolFilename =
+    process.platform === 'win32' ? `${toolName}.exe` : toolName
+  let toolPath: string
+
+  try {
+    toolPath = await cache.downloadTool(version.url)
+  } catch (err) {
+    core.debug(err)
+    throw new Error(`Failed to download version ${version.target}: ${err}`)
+  }
+
+  if (version.url.endsWith('.tar.gz')) {
+    toolPath = await cache.extractTar(toolPath)
+    toolPath = path.join(toolPath, toolFilename)
+  }
+
+  switch (process.platform) {
+    case 'linux':
+    case 'darwin':
+      fs.chmodSync(toolPath, 0o755)
+      break
+  }
+
+  return await cache.cacheFile(toolPath, toolFilename, toolName, version.target)
+}
